5628a282d45e2876b51c13f252f8eb05eb041101
Illeana Planzo <illeanaplanzo@hotmail.com>
2021-05-17 16:23:28 -0300

    adicionando repositories

---
+++ src/main/java/com/illeanaplanzo/pontointeligente/api/repositories/EmpresaRepository.java
@@ -0,0 +1,13 @@
+package com.illeanaplanzo.pontointeligente.api.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+import com.illeanaplanzo.pontointeligente.api.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
