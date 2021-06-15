c7f8e3c5195a8074c4697a8692d08e4ec6d240b4
Illeana Planzo <illeanaplanzo@hotmail.com>
2021-06-15 09:14:59 -0300

    adicionado service de funcionario

---
+++ src/test/java/com/illeanaplanzo/pontointeligente/api/services/FuncionarioServiceTest.java
@@ -0,0 +1,69 @@
+package com.illeanaplanzo.pontointeligente.api.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.illeanaplanzo.pontointeligente.api.entities.Funcionario;
+import com.illeanaplanzo.pontointeligente.api.repositories.FuncionarioRepository;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class FuncionarioServiceTest {
+	
+	@MockBean
+	private FuncionarioRepository funcionarioRepository;
+	
+	@Autowired
+	private FuncionarioService funcionarioService;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		BDDMockito.given(this.funcionarioRepository.save(Mockito.any(Funcionario.class))).willReturn(new Funcionario());
+	//	BDDMockito.given(this.funcionarioRepository.findById(Mockito.anyLong())).thenReturn(new Funcionario());
+		BDDMockito.given(this.funcionarioRepository.findByEmail(Mockito.anyString())).willReturn(new Funcionario());
+		BDDMockito.given(this.funcionarioRepository.findByCpf(Mockito.anyString())).willReturn(new Funcionario());
+	}
+	
+	@Test
+	public void testPersistirFuncionario() {
+		Funcionario funcionario = this.funcionarioService.persistir(new Funcionario());
+		
+		Assertions.assertNotNull(funcionario);
+	}
+	
+ /*	problema com funcao thenReturn do BDDMockito para return Optional
+  * @Test
+	public void testBuscarFuncionarioPorId() {
+		Optional<Funcionario> funcionario = this.funcionarioService.buscarPorId(1L);
+		
+		Assertions.assertTrue(funcionario.isPresent());
+	}
+	
+	public void testBuscarFuncionarioPorEmail() {
+		Optional<Funcionario> funcionario = this.funcionarioService.buscarPorEmail("email@email.com");
+		
+		Assertions.assertTrue(funcionario.isPresent());
+	}
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
